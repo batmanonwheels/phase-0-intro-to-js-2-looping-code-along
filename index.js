e9f9414e696d955f0e48c9cb8f5632289b034e0f
Devin <devingarcia5423@gmail.com>
2022-05-03 18:30:30 -0400

    commit tree 6bdd23daa790fd3ee94c1506e8da2a1bd3d9cfec

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const messages = [];
+
+function writeCards(array, event) {
+    for (let i = 0; i < array.length;  i++) {
+        messages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    };
+    return messages;
+};
+
+function countDown() {
+    let i = 10;
+    while (i >= 0 ) {
+        console.log(i);
+        i--;
+    };
+};
